fbb39fb7effc9bbf8954bf83634b0b4655e4b817
Klesfot <mukhamediev.l@gmail.com>
2020-05-09 16:04:52 +0300

    Start python autotest project

---
+++ WebDriverTests/tests.py
@@ -0,0 +1,66 @@
+def chorus():
+    print("")
+    print("Never gonna give you up")
+    print("Never gonna let you down")
+    print("Never gonna run around and desert you")
+    print("Never gonna make you cry")
+    print("Never gonna say goodbye")
+    print("Never gonna tell a lie and hurt you")
+    print("")
+
+
+def vocals():
+    print("")
+    print("Oooooh, give you up")
+    print("")
+
+
+def give():
+    print("")
+    print("Give you up")
+    print("")
+
+
+print("We're no strangers to love")
+print("You know the rules and so do i")
+print("A full commitment's what i'm thinking of")
+print("You wouldn't get this from any other guy")
+print("I just want to tell you how i'm feeling")
+print("Gotta make you understand")
+
+chorus()
+
+print("We've know each other for so long")
+print("Your heart's been aching but you're just shy to say it")
+print("Inside we both know what's been going on")
+print("We know the game and we'll gonna play it")
+print("And if you ask me how i'm feeling")
+print("Don't tell me you're too blind do see")
+
+chorus()
+
+chorus()
+
+vocals()
+vocals()
+
+print("Never gonna give, never gonna give")
+
+give()
+
+print("Never gonna give, never gonna give")
+
+give()
+
+print("We've know each other for so long")
+print("Your heart's been aching but you're just shy to say it")
+print("Inside we both know what's been going on")
+print("We know the game and we'll gonna play it")
+print("I just want to tell you how i'm feeling")
+print("Got to make you understand")
+
+chorus()
+
+chorus()
+
+chorus()
